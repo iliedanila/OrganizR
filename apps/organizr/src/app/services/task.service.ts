53bf0261dfa2a774d987e85c391377bd9c8157ab
Ilie Danila <ilie.danila@proton.me>
2024-06-02 23:37:31 +0300

    Add nx to project

---
+++ apps/organizr/src/app/services/task.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import {
+  Firestore,
+  collection,
+  collectionData,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
+import { Task } from "@organizr/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TaskService {
+  constructor(private firestore: Firestore) {}
+
+  getTasks(): Observable<Task[]> {
+    const tasksCollection = collection(this.firestore, "tasks");
+    return collectionData(tasksCollection, { idField: "id" }) as Observable<
+      Task[]
+    >;
+  }
+
+  addTask(task: Task): Promise<void> {
+    const tasksCollection = collection(this.firestore, "tasks");
+    return addDoc(tasksCollection, task).then(() => {}); // Return void
+  }
+
+  deleteTask(taskId: string): Promise<void> {
+    const taskDoc = doc(this.firestore, `tasks/${taskId}`);
+    return deleteDoc(taskDoc);
+  }
+}
---
+++ libs/core/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./interfaces/user.interface";
+export * from "./interfaces/user.service";
+export * from "./interfaces/task.interface";
+export * from "./interfaces/task.service";
